25b3b5b91f31a6131986233d976c4d77068b9329
Anastasiia Riumina <anastasiapavlenko323@gmail.com>
2025-07-20 18:34:59 +0300

    add markup

--- js/gallery.js
+++ js/gallery.js
@@ -64,3 +64,38 @@ const images = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = images.map(({preview, original, description}) => {
+  return`
+  <li class="gallery-item">
+      <a class="gallery-link" href="${original}">
+        <img
+          class="gallery-image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+}).join('');
+
+galleryContainer.innerHTML = galleryMarkup;
+
+ galleryContainer.addEventListener('click', event => {
+   event.preventDefault();
+
+  const clickedImage = event.target;
+  if (clickedImage.nodeName !== 'IMG') return;
+
+  const largeImageURL = clickedImage.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImageURL}" alt="${clickedImage.alt}" />
+  `);
+
+  instance.show();
+});
+
